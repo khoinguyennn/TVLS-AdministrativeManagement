36ac75a5db7114256a62f266fc9a7b1ff2a97b5c
khoinguyennn <nhoknguyencutehj@gmail.com>
2026-03-05 10:11:46 +0700

    Implement code changes to enhance functionality and improve performance

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -57,7 +57,11 @@ export class App {
     this.app.use(morgan(LOG_FORMAT, { stream }));
     this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
     this.app.use(hpp());
-    this.app.use(helmet());
+    this.app.use(
+      helmet({
+        crossOriginResourcePolicy: { policy: 'cross-origin' },
+      }),
+    );
     this.app.use(compression());
     this.app.use(express.json({ limit: '10mb' }));
     this.app.use(express.urlencoded({ extended: true, limit: '10mb' }));
